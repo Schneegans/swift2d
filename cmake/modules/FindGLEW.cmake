5aae92a12f430d4447ddad145b3be6263a4e8f60
Simon Schneegans <code@simonschneegans.de>
2014-09-25 18:14:22 +0200

    The library links. Now on to the executable...

--- cmake/modules/FindGLEW.cmake
+++ cmake/modules/FindGLEW.cmake
@@ -7,7 +7,7 @@ FIND_PATH(GLEW_INCLUDE_DIRS GL/glew.h DOC "Path to GLEW include directory."
 )
 
 #Search for the library file...
-SET(GLEW_LIB_NAMES GLEW glew32s.lib)
+SET(GLEW_LIB_NAMES GLEW glew32mx.lib glew32mxs.lib)
 
 FIND_LIBRARY(GLEW_LIBRARIES DOC "Absolute path to GLEW library."
   NAMES ${GLEW_LIB_NAMES}
