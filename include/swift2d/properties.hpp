ebe129915a742fcfd56dc9608b54ebe94f4b0404
Simon Schneegans <code@simonschneegans.de>
2015-02-09 17:44:40 +0100

    use RakString

--- include/swift2d/properties.hpp
+++ include/swift2d/properties.hpp
@@ -12,32 +12,34 @@
 #include <swift2d/properties/Property.hpp>
 #include <swift2d/properties/AnimatedProperty.hpp>
 
+#include <raknet/RakString.h>
 #include <string>
 
 namespace swift {
 
-typedef SWIFT_DLL Property<double>         Double;
-typedef SWIFT_DLL Property<float>          Float;
-typedef SWIFT_DLL Property<math::int8>     Int8;
-typedef SWIFT_DLL Property<math::int16>    Int16;
-typedef SWIFT_DLL Property<math::int32>    Int32;
-typedef SWIFT_DLL Property<math::int64>    Int64;
-typedef SWIFT_DLL Property<math::uint8>    UInt8;
-typedef SWIFT_DLL Property<math::uint16>   UInt16;
-typedef SWIFT_DLL Property<math::uint32>   UInt32;
-typedef SWIFT_DLL Property<math::uint64>   UInt64;
+typedef SWIFT_DLL Property<double>              Double;
+typedef SWIFT_DLL Property<float>               Float;
+typedef SWIFT_DLL Property<math::int8>          Int8;
+typedef SWIFT_DLL Property<math::int16>         Int16;
+typedef SWIFT_DLL Property<math::int32>         Int32;
+typedef SWIFT_DLL Property<math::int64>         Int64;
+typedef SWIFT_DLL Property<math::uint8>         UInt8;
+typedef SWIFT_DLL Property<math::uint16>        UInt16;
+typedef SWIFT_DLL Property<math::uint32>        UInt32;
+typedef SWIFT_DLL Property<math::uint64>        UInt64;
 
-typedef SWIFT_DLL Property<math::vec3>     Vec3;
-typedef SWIFT_DLL Property<math::vec3i>    Vec3i;
-typedef SWIFT_DLL Property<math::vec3ui>   Vec3ui;
-typedef SWIFT_DLL Property<math::vec2>     Vec2;
-typedef SWIFT_DLL Property<math::vec2i>    Vec2i;
-typedef SWIFT_DLL Property<math::vec2ui>   Vec2ui;
-typedef SWIFT_DLL Property<math::mat3>     Mat3;
+typedef SWIFT_DLL Property<math::vec3>          Vec3;
+typedef SWIFT_DLL Property<math::vec3i>         Vec3i;
+typedef SWIFT_DLL Property<math::vec3ui>        Vec3ui;
+typedef SWIFT_DLL Property<math::vec2>          Vec2;
+typedef SWIFT_DLL Property<math::vec2i>         Vec2i;
+typedef SWIFT_DLL Property<math::vec2ui>        Vec2ui;
+typedef SWIFT_DLL Property<math::mat3>          Mat3;
 
-typedef SWIFT_DLL Property<bool>           Bool;
+typedef SWIFT_DLL Property<bool>                Bool;
 
-typedef SWIFT_DLL Property<std::string>    String;
+typedef SWIFT_DLL Property<std::string>         String;
+typedef SWIFT_DLL Property<RakNet::RakString>   NetString;
 
 }
 
