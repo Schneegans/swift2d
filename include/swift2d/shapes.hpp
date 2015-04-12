ed7b9092f99908c7ba6facdab1af402c68a2e257
Simon Schneegans <code@simonschneegans.de>
2015-04-12 15:14:14 +0200

    added MIT license

--- include/swift2d/shapes.hpp
+++ include/swift2d/shapes.hpp
@@ -4,6 +4,9 @@
 //                                                                            //
 // Copyright: (c) 2011-2015 Simon Schneegans & Felix Lauer                    //
 //                                                                            //
+// This software may be modified and distributed under the terms              //
+// of the MIT license.  See the LICENSE file for details.                     //
+//                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
 #ifndef SWIFT2D_INCLUDE_SHAPES_HPP
--- src/swift2d/components/DealDamageComponent.cpp
+++ src/swift2d/components/DealDamageComponent.cpp
@@ -4,6 +4,9 @@
 //                                                                            //
 // Copyright: (c) 2011-2015 Simon Schneegans & Felix Lauer                    //
 //                                                                            //
+// This software may be modified and distributed under the terms              //
+// of the MIT license.  See the LICENSE file for details.                     //
+//                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <swift2d/components/DealDamageComponent.hpp>
